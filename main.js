90c9de0cc300c6db1ceb496c4699888b961b98f2
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-06 16:58:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ function preload() {
 function setup() {
  canvas=createCanvas(450,450);
  canvas.center();
+video=createCapture(VIDEO);
+video.size(450,450);
+video.hide();
+poseNet=ml5.poseNet(video,modelloaded)
+poseNet.on('pose',got_poses)
+}
+function modelloaded() {
+console.log("modelloaded");
+}
 
+function got_poses(results) {
+if( results.length>0 ){
+    console.log(results);
+    console.log("nose x="+ results[0].pose.nose.x);
+    console.log("nose y="+ results[0].pose.nose.y);
+}
 }
 
 function draw() {
-
+image(video,0,0,450,450)
 }
 
 function take_snapshot() {
-    save("mustache picture.png")
+    save("moustache_picture.png")
 }
