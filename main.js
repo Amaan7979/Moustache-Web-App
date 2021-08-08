e423dc9c00032c53535569ab40930cd452144eaf
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-08 16:45:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload() {
-
+    mustache=loadImage("https://i.postimg.cc/bv0Fvj11/mustache.png");
 }
 function setup() {
  canvas=createCanvas(450,450);
@@ -16,6 +18,8 @@ console.log("modelloaded");
 
 function got_poses(results) {
 if( results.length>0 ){
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
     console.log(results);
     console.log("nose x="+ results[0].pose.nose.x);
     console.log("nose y="+ results[0].pose.nose.y);
@@ -23,7 +27,8 @@ if( results.length>0 ){
 }
 
 function draw() {
-image(video,0,0,450,450)
+image(video,0,0,450,450);
+image(mustache,noseX-50,noseY-25,125,125);
 }
 
 function take_snapshot() {
